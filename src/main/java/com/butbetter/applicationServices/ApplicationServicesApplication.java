64c87c21ffea2ff40d728bf4876088390f5e77c8
kev <kevHTW@gmx.de>
2021-11-02 12:34:39 +0100

    init project

---
+++ src/main/java/com/butbetter/applicationServices/ApplicationServicesApplication.java
@@ -0,0 +1,13 @@
+package com.butbetter.applicationServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationServicesApplication.class, args);
+	}
+
+}
