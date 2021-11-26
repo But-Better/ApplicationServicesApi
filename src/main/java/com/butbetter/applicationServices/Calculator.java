b63ef210e8c10141cdfc97f0f40932b6ffc6cebb
MathisHTW <Mathis.Schott@Student.HTW-Berlin.de>
2021-11-26 21:03:14 +0100

    add logging

--- src/main/java/com/butbetter/applicationServices/Calculator.java
+++ src/main/java/com/butbetter/applicationServices/Calculator.java
@@ -1,28 +1,45 @@
 package com.butbetter.applicationServices;
 
 import com.butbetter.applicationServices.model.CalcApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class Calculator implements CalculatorService{
 
-    private final String CALC_API_URL_START;
-    private final String CALC_API_URL_END;
+    private  String CALC_API_URL_START = "https://caluationapi.herokuapp.com/calc/v1/VAT?price=";
+    private  String CALC_API_URL_END = "&percent=";
+
+    private static final Logger log = LoggerFactory.getLogger(Calculator.class);
+
+    public String getCALC_API_URL_START(){
+        log.info("urlStart : " + CALC_API_URL_START);
+        return this.CALC_API_URL_START;
+    }
+
+    public String getCALC_API_URL_END() {
+        log.info("urlEnd : " + CALC_API_URL_END);
+        return CALC_API_URL_END;
+    }
+
+    public void setCALC_API_URL_START(String CALC_API_URL_START) {
+        this.CALC_API_URL_START = CALC_API_URL_START;
+        log.info("urlStart : " + CALC_API_URL_START);
+    }
+
+    public void setCALC_API_URL_END(String CALC_API_URL_END) {
+        this.CALC_API_URL_END = CALC_API_URL_END;
+        log.info("urlEnd : " + CALC_API_URL_END);
+    }
 
     private RestTemplate calcAPIRESTTemplate= new RestTemplate();
-
-    public Calculator(){
-        this.CALC_API_URL_START = "https://caluationapi.herokuapp.com/calc/v1/VAT?price=";
-        this.CALC_API_URL_END = "&percent=";
-    }
-
-    public Calculator(String urlStart, String urlEnd){
-        this.CALC_API_URL_START = urlStart;
-        this.CALC_API_URL_END = urlEnd;
-    }
 
     @Override
     public float calculateVATofPrice(float price, float percent) {
         String restReq = this.CALC_API_URL_START + price + this.CALC_API_URL_END + percent;
+        log.info("accessing [" + restReq + "] to calc VAT of " + price + " with VAT(" + percent + "%)");
         CalcApiResponse calcApiResponse = calcAPIRESTTemplate.getForObject(restReq, CalcApiResponse.class);
         return Float.parseFloat(calcApiResponse.getVatResult());
     }
