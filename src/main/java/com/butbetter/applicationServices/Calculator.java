2dd66bd18e11ca3a30f582d5f42e5e60c75d3176
MathisHTW <Mathis.Schott@Student.HTW-Berlin.de>
2021-12-01 16:07:08 +0100

    rename var for convention style

--- src/main/java/com/butbetter/applicationServices/Calculator.java
+++ src/main/java/com/butbetter/applicationServices/Calculator.java
@@ -4,41 +4,43 @@ import com.butbetter.applicationServices.model.CalcApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public class Calculator implements CalculatorService{
+public class Calculator implements CalculatorService {
 
-    private  String CALC_API_URL_START = "https://caluationapi.herokuapp.com/calc/v1/VAT?price=";
-    private  String CALC_API_URL_END = "&percent=";
+    private String calcApiUrlStart = "https://caluationapi.herokuapp.com/calc/v1/VAT?price=";
+    private String calcApiUrlEnd = "&percent=";
 
     private static final Logger log = LoggerFactory.getLogger(Calculator.class);
 
-    public String getCALC_API_URL_START(){
-        log.info("urlStart : " + CALC_API_URL_START);
-        return this.CALC_API_URL_START;
+    public String getCalcApiUrlStart() {
+        log.info("urlStart : " + calcApiUrlStart);
+        return this.calcApiUrlStart;
     }
 
-    public String getCALC_API_URL_END() {
-        log.info("urlEnd : " + CALC_API_URL_END);
-        return CALC_API_URL_END;
+    public String getCalcApiUrlEnd() {
+        log.info("urlEnd : " + calcApiUrlEnd);
+        return calcApiUrlEnd;
     }
 
-    public void setCALC_API_URL_START(String CALC_API_URL_START) {
-        this.CALC_API_URL_START = CALC_API_URL_START;
-        log.info("urlStart set to: " + CALC_API_URL_START);
+    public void setCalcApiUrlStart(String calcApiUrlStart) {
+        this.calcApiUrlStart = calcApiUrlStart;
+        log.info("urlStart set to: " + calcApiUrlStart);
     }
 
-    public void setCALC_API_URL_END(String CALC_API_URL_END) {
-        this.CALC_API_URL_END = CALC_API_URL_END;
-        log.info("urlEnd set to: " + CALC_API_URL_END);
+    public void setCalcApiUrlEnd(String calcApiUrlEnd) {
+        this.calcApiUrlEnd = calcApiUrlEnd;
+        log.info("urlEnd set to: " + calcApiUrlEnd);
     }
 
-    private RestTemplate calcAPIRESTTemplate= new RestTemplate();
+    private RestTemplate calcAPIRESTTemplate = new RestTemplate();
 
     @Override
-    public float calculateVATofPrice(float price, float percent) {
-        String restReq = this.CALC_API_URL_START + price + this.CALC_API_URL_END + percent;
+    public float calculateVATofPrice(float price, float percent) throws ResourceAccessException, HttpClientErrorException.BadRequest {
+        String restReq = this.calcApiUrlStart + price + this.calcApiUrlEnd + percent;
         log.info("accessing [" + restReq + "] to calc VAT of " + price + " with VAT(" + percent + "%)");
         CalcApiResponse calcApiResponse = calcAPIRESTTemplate.getForObject(restReq, CalcApiResponse.class);
         return Float.parseFloat(calcApiResponse.getVatResult());
