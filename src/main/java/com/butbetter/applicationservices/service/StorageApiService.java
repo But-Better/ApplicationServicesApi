f64457a38a27f1be3cf03bf3b01ddc845d27831d
kev <kevHTW@gmx.de>
2021-12-03 18:46:46 +0100

    BB-31 add StorageApiService.java

---
+++ src/main/java/com/butbetter/applicationservices/service/StorageApiService.java
@@ -0,0 +1,11 @@
+package com.butbetter.applicationservices.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StorageApiService {
+
+
+    private static final String HEROKU_URL = "";
+
+}
