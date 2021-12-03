887fb7b0e0023ab9d8af99673ab9a7cbcf6d7586
MathisHTW <Mathis.Schott@Student.HTW-Berlin.de>
2021-12-03 11:59:09 +0100

    remove comments in interface

--- src/main/java/com/butbetter/applicationServices/CalculatorService.java
+++ src/main/java/com/butbetter/applicationServices/CalculatorService.java
@@ -5,9 +5,5 @@ import org.springframework.web.client.ResourceAccessException;
 
 public interface CalculatorService {
 
-    //public float calculateVATofProduct(Product product, float percent);
-
-    //public float calculateVATofProducts(List<Product> products, float percent);
-
     float calculateVATofPrice(float price, float percent) throws ResourceAccessException, HttpClientErrorException.BadRequest;
 }
