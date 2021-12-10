10b0436311e89524b6f8a58e40fd66ae8b186a3e
MathisHTW <Mathis.Schott@Student.HTW-Berlin.de>
2021-12-10 20:34:36 +0100

    add language enum

---
+++ src/main/java/com/butbetter/applicationservices/Language.java
@@ -0,0 +1,39 @@
+package com.butbetter.applicationservices;
+
+public enum Language {
+    DE("German"),
+    EN("English"),
+    BG("Bulgarian"),
+    CS("Czech"),
+    DA("Danish"),
+    EL("Greek"),
+    ES("Spanish"),
+    ET("Estonian"),
+    FI("Finnish"),
+    FR("French"),
+    HU("Hungarian"),
+    IT("Italian"),
+    JA("Japanese"),
+    LT("Lithuanian"),
+    LV("Latvian"),
+    NL("Dutch"),
+    PL("Polish"),
+    PT("Portuguese"),
+    RO("Romanian"),
+    RU("Russian"),
+    SK("Slovak"),
+    SL("Slovenian"),
+    SV("Swedish"),
+    ZH("Chinese");
+
+    private String fullLanguage;
+
+    Language(String language){
+        this.fullLanguage = language;
+    }
+
+    public String getFullLanguage() {
+        return fullLanguage;
+    }
+
+}
