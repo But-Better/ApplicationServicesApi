17b50936c150eb27a1f3cd11aa05c00a25ccbb72
Mathis <mathis.schott@gmail.com>
2021-12-17 19:10:19 +0100

    BB-45 add translator tests

--- src/test/java/com/butbetter/applicationservices/TranslatorTest.java
+++ src/test/java/com/butbetter/applicationservices/TranslatorTest.java
@@ -18,4 +18,17 @@ class TranslatorTest {
     void testGermanToEnglishTranslation(){
         assertEquals("Friend",translator.translate("Freund",Language.EN));
     }
+
+    @Test
+    @DisplayName("test german to english translation of Mein Freund läuft schnell.")
+    void testGermanToEnglishTranslationSentence(){
+        assertEquals("My friend runs fast.",translator.translate("Mein Freund läuft schnell.",Language.EN));
+    }
+
+    @Test
+    @DisplayName("Test the language of a given word: Freund")
+    void testLanguageTypeDetection(){
+        assertEquals(Language.DE,translator.getLanguage("Freund"));
+    }
+
 }
