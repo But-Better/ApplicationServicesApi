ba1bb58114578ab2b8137a03990ca84fa1e86087
notefox <kitsu-geissler@t-online.de>
2022-01-13 14:15:13 +0100

    BB-9 remove not finished artifacts from BB-1

--- src/main/java/com/butbetter/applicationservices/db/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.butbetter.applicationservices.db.repository;
-
-import com.butbetter.applicationservices.db.model.Product;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.UUID;
-
-public interface ProductRepository extends CrudRepository<Product, UUID> {
-}
