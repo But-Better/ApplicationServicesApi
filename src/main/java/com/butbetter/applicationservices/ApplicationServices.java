4cbc4c8033cbc8d14ba77f476a1521a28b6b8700
kev <kevHTW@gmx.de>
2022-01-16 16:14:02 +0100

    BB-47 EnableCaching

--- src/main/java/com/butbetter/applicationservices/ApplicationServices.java
+++ src/main/java/com/butbetter/applicationservices/ApplicationServices.java
@@ -1,11 +1,11 @@
 package com.butbetter.applicationservices;
 
-import org.hibernate.cfg.Environment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ApplicationServices {
 
 	public static void main(String[] args) {
