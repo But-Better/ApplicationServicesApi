a8320ce27391da89d4b234bba58c872bbee66cc4
kev <kevHTW@gmx.de>
2022-01-16 16:19:56 +0100

    BB-53 add Repository stereotype

--- src/main/java/com/butbetter/applicationservices/db/repository/ProductRepository.java
+++ src/main/java/com/butbetter/applicationservices/db/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package com.butbetter.applicationservices.db.repository;
 
 import com.butbetter.applicationservices.db.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ProductRepository extends CrudRepository<Product, UUID> {
 }
