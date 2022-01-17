785d13b99ebccf9e7e4fee73118a05a819831521
kev <kevHTW@gmx.de>
2022-01-17 02:09:56 +0100

    BB-53 cleaning up

--- src/main/java/com/butbetter/applicationservices/db/service/ProductValidationService.java
+++ src/main/java/com/butbetter/applicationservices/db/service/ProductValidationService.java
@@ -1,8 +1,6 @@
 package com.butbetter.applicationservices.db.service;
 
 import com.butbetter.applicationservices.db.model.Product;
-import com.butbetter.applicationservices.db.model.ProductBeverageEnum;
-import com.butbetter.applicationservices.db.model.ProductRatingEnum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
