1bba6ceb305b025b4d781c74f2198c83175dd1b4
note <kitsu-geissler@t-online.de>
2022-01-28 23:09:39 +0100

    BB-44 remove old CSVProductInformationExporter.java

--- src/main/java/com/butbetter/applicationservices/csvExporter/CSVProductInformationExporter.java
+++
@@ -1,5 +0,0 @@
-package com.butbetter.applicationservices.csvExporter;
-
-import com.butbetter.applicationservices.model.ProductInformation;
-
-interface CSVProductInformationExporter extends CSVExporter<ProductInformation> {}
