3cf0d4ce8404416a7f6495c0d1d588fe8c1512c4
Notefox <kitsu-geissler@t-online.de>
2022-03-20 12:38:40 +0100

    BB-42 add conversion test for a ProductInformation Collection

--- src/test/java/com/butbetter/applicationservices/csvExporter/csvConverter/CSVConverterTest.java
+++ src/test/java/com/butbetter/applicationservices/csvExporter/csvConverter/CSVConverterTest.java
@@ -2,15 +2,14 @@ package com.butbetter.applicationservices.csvExporter.csvConverter;
 
 import com.butbetter.applicationservices.storagerestapi.model.Address;
 import com.butbetter.applicationservices.storagerestapi.model.ProductInformation;
-import com.opencsv.exceptions.CsvDataTypeMismatchException;
-import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -25,7 +24,53 @@ class CSVConverterTest {
 
     @SneakyThrows
     @Test
-    void convertSingle() throws CsvRequiredFieldEmptyException, CsvDataTypeMismatchException, IOException {
+    void testConvertSingleGeneral() {
+        ProductInformation testInformation = generateValidProductInformation();
+
+        String csv = converter.convertSingle(testInformation);
+
+        String expectedCsv = "\"ADDRESS\"\n"+
+                "\"AMOUNT\"\n"+
+                "\"DELIVERYTIME\"\n"+
+                "\"UUID\"\n"+
+                generateCSVStructure(testInformation);
+
+        generateCSVStructure(testInformation);
+
+        assertEquals(csv, expectedCsv);
+    }
+
+    @SneakyThrows
+    @Test
+    void testConvertMultipleGeneral() {
+
+        ArrayList<ProductInformation> testInformationCollection = new ArrayList<>();
+
+        for (int i = 0; i < 50; i++) {
+            testInformationCollection.add(generateValidProductInformation());
+        }
+
+        String csv = converter.convertList(testInformationCollection);
+
+        String expectedCsv = "\"ADDRESS\"\n"+
+                "\"AMOUNT\"\n"+
+                "\"DELIVERYTIME\"\n"+
+                "\"UUID\"\n"+
+                testInformationCollection.stream()
+                        .map(this::generateCSVStructure)
+                        .collect(Collectors.joining());
+
+        assertEquals(csv, expectedCsv);
+    }
+
+    private String generateCSVStructure(ProductInformation testInformation) {
+        return  "\""+ testInformation.getAddress()      +"\"\n"+
+                "\""+ testInformation.getAmount()       +"\"\n"+
+                "\""+ testInformation.getDeliveryTime() +"\"\n"+
+                "\""+ testInformation.getUuid()         +"\"\n";
+    }
+
+    private ProductInformation generateValidProductInformation() {
         Address address = new Address("name", "companyName", "street", "city", "12345", "country");
         address.setId(UUID.randomUUID().toString());
         String dateTime = OffsetDateTime.now().toString();
@@ -37,18 +82,6 @@ class CSVConverterTest {
 
         String uuid = UUID.randomUUID().toString();
         testInformation.setUuid(uuid);
-
-        String csv = converter.convertSingle(testInformation);
-
-        String expectedCsv = "\"ADDRESS\"\n"+
-                "\"AMOUNT\"\n"+
-                "\"DELIVERYTIME\"\n"+
-                "\"UUID\"\n\""+
-                address +"\"\n"+
-                "\"0\"\n\""+
-                dateTime+"\"\n"+
-                "\"" + uuid + "\"\n";
-
-        assertEquals(csv, expectedCsv);
+        return testInformation;
     }
 }
